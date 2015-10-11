dd5c7606c13700b88125f69dbc9d4a77bafbb18d
Faisal Shaik <fshaik@LCLaptop78.local>
2015-10-11 12:50:00 -0700

    started splash page

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ mongoose.connection.once('open', function() {
   var routes = require('./routes');
   _.each(routes, function(controller, route) {
     app.use(route, controller(app, route));
-  });
+   });
 
   console.log('Listening on port 3000...');
   app.listen(3000);
